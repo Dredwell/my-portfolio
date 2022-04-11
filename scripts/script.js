39710659fa8fa41af4e4dc5c060abe08907cbc9c
Dredwell <dredwell.velasquez@gmail.com>
2022-04-11 14:27:52 -0600

    Added Linters and events to nav elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const menuButton = document.getElementById('menu-button');
 const mobileMenu = document.getElementById('mobile-menu');
 const exitButton = document.getElementById('exit-button');
+const navElements = document.querySelectorAll('.nav-elements a');
 
 menuButton.addEventListener('click',function() {
     mobileMenu.style.display = 'flex';
@@ -9,3 +10,11 @@ menuButton.addEventListener('click',function() {
 exitButton.addEventListener('click',function() {
     mobileMenu.style.display = 'none';
 })
+
+console.log(navElements);
+
+for(let i = 0; i < navElements.length; i++) {
+    navElements[i].addEventListener('click', function() {
+        mobileMenu.style.display = 'none';
+    })
+}
