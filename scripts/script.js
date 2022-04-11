58134cf0024a1301be34c3c92258c08ea79dcb43
Dredwell <dredwell.velasquez@gmail.com>
2022-04-11 14:35:44 -0600

    fixed linters errors

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,16 @@ const mobileMenu = document.getElementById('mobile-menu');
 const exitButton = document.getElementById('exit-button');
 const navElements = document.querySelectorAll('.nav-elements a');
 
-menuButton.addEventListener('click',function() {
-    mobileMenu.style.display = 'flex';
+menuButton.addEventListener('click', () => {
+  mobileMenu.style.display = 'flex';
 });
 
-exitButton.addEventListener('click',function() {
-    mobileMenu.style.display = 'none';
+exitButton.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
 })
 
-console.log(navElements);
-
 for(let i = 0; i < navElements.length; i++) {
-    navElements[i].addEventListener('click', function() {
-        mobileMenu.style.display = 'none';
-    })
+  navElements[i].addEventListener('click', () => {
+      mobileMenu.style.display = 'none';
+  })
 }
