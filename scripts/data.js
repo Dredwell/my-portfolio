445880571f7ba065ff992e952c6bb54c80c06587
Dredwell <dredwell.velasquez@gmail.com>
2022-04-12 13:41:51 -0600

    Added data.js

---
+++ scripts/data.js
@@ -0,0 +1,48 @@
+export let dataProjects = [{
+  imgUrl: './media/Snapshoot-Portfolio.png',
+  projectTitle: 'Keeping track of hundreds of components #1',
+  tags: ['Ruby on Rails','JavaScript', 'CSS'],
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ullam repudiandae nesciunt natus vitae a commodi, dicta optio ex dolor amet quo odio nam voluptatum magnam repellendus deleniti veritatis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus distinctio maiores, reiciendis tempora asperiores nulla odit quo vero veritatis ex magnam quos, alias, ipsa officia. Voluptate doloremque natus iste maiores!',
+  liveUrl: 'dredwell.github.io/my-portfolio',
+  sourceUrl: 'https://github.com/Dredwell/my-portfolio'
+},
+{
+  imgUrl: './media/Snapshoot-Portfolio.png',
+  projectTitle: 'Keeping track of hundreds of components #2',
+  tags: ['Ruby on Rails','JavaScript', 'CSS', 'HTML'],
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ullam repudiandae nesciunt natus vitae a commodi, dicta optio ex dolor amet quo odio nam voluptatum magnam repellendus deleniti veritatis obcaecati?',
+  liveUrl: 'dredwell.github.io/my-portfolio',
+  sourceUrl: 'https://github.com/Dredwell/my-portfolio'
+},
+{
+  imgUrl: './media/github.svg',
+  projectTitle: 'Keeping track of hundreds of components #3',
+  tags: ['Ruby on Rails','JavaScript', 'CSS', 'Ruby'],
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ullam repudiandae nesciunt natus vitae a commodi, dicta optio ex dolor amet quo odio nam voluptatum magnam repellendus deleniti veritatis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus distinctio maiores, reiciendis tempora asperiores nulla odit quo vero veritatis ex magnam quos, alias, ipsa officia. Voluptate doloremque natus iste maiores!',
+  liveUrl: 'dredwell.github.io/my-portfolio',
+  sourceUrl: 'https://github.com/Dredwell/my-portfolio'
+},
+{
+  imgUrl: './media/Snapshoot-Portfolio.png',
+  projectTitle: 'Keeping track of hundreds of components #4',
+  tags: ['Ruby on Rails','JavaScript', 'CSS'],
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ullam repudiandae nesciunt natus vitae a commodi, dicta optio ex dolor amet quo odio nam voluptatum magnam repellendus deleniti veritatis obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus distinctio maiores, reiciendis tempora asperiores nulla odit quo vero veritatis ex magnam quos, alias, ipsa officia. Voluptate doloremque natus iste maiores!',
+  liveUrl: 'dredwell.github.io/my-portfolio',
+  sourceUrl: 'https://github.com/Dredwell/my-portfolio'
+},
+{
+  imgUrl: './media/Snapshoot-Portfolio.png',
+  projectTitle: 'Keeping track of hundreds of components #5',
+  tags: ['Ruby on Rails','JavaScript', 'CSS'],
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ullam repudiandae nesciunt natus vitae a commodi, dicta optio ex dolor amet quo odio nam voluptatum magnam repellendus deleniti veritatis obcaecati?',
+  liveUrl: 'dredwell.github.io/my-portfolio',
+  sourceUrl: 'https://github.com/Dredwell/my-portfolio'
+},
+{
+  imgUrl: './media/Snapshoot-Portfolio.png',
+  projectTitle: 'Keeping track of hundreds of components #6',
+  tags: ['Ruby on Rails','JavaScript', 'CSS'],
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ullam repudiandae nesciunt natus vitae a commodi, dicta optio ex dolor amet quo odio nam voluptatum magnam repellendus deleniti veritatis obcaecati?',
+  liveUrl: 'dredwell.github.io/my-portfolio',
+  sourceUrl: 'https://github.com/Dredwell/my-portfolio'
+}]
