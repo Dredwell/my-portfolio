e5677b82c023bff616a4b6231bddd946da437b23
AntonioHincapie <marcohincapiem@gmail.com>
2022-04-12 13:20:44 -0500

    Added the events to the projects.
    -Added the ids for the functions.
    -Added the script for this functions.

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,18 @@ for (let i = 0; i < navElements.length; i += 1) {
     mobileMenu.style.display = 'none';
   });
 }
+
+const projectButtons = document.getElementsByClassName('see-project-button');
+const popupWindow = document.getElementById('popup-window');
+const closeWindow = document.getElementById('close-icon');
+
+closeWindow.addEventListener('click', () => {
+  popupWindow.style.display = 'none';
+});
+
+for (let i = 0; i < projectButtons.length; i += 1) {
+  projectButtons[i].addEventListener('click', () => {
+    popupWindow.style.top = `${window.scrollY}px`;
+    popupWindow.style.display = 'flex';
+  });
+}
